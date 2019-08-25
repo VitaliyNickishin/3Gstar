66d4bbc88cc99e5046a2f244b73fd484351ec127
“VitaliyNickishin” <nickishin34@gmail.com>
2019-08-25 22:50:05 +0300

    section reviews is ready

--- js/main.js
+++ js/main.js
@@ -4,3 +4,38 @@ $('.navbar-toggler').on('click', function(e) {
  $(this).toggleClass('navbar-toggler_active');//находим.navbar-toggler и создаем класс navbar-toggler_active для переварачивания полосок
  
 });
+
+/*слайдер slick для блока reviews*/
+$(".reviews-slider").slick({
+ draggable: true, //отключаем перелистывание пальцем
+ //dots: true, //отобразим точки для слайда
+//dotsClass: 'slick-dots reviews-slider__dots', //присваиваем точкам наш класс или оставляеа по умолчанию slick-dots
+ slidesToShow: 3, //отображеть слайдов
+ slidesToScroll: 1, //перелистывать слайд
+ responsive: [ //для медиа запросов
+  {
+    breakpoint: 768,
+    settings: {
+     slidesToShow: 2,
+     slidesToScroll: 1,
+     infinite: true,
+     prevArrow: "<button type='button' class='arrow slick-prev'></button>",
+     nextArrow: "<button type='button' class='arrow slick-next'></button>",
+     dots: true, //убираем точки
+     dotsClass: 'slick-dots reviews-slider__dots', //присваиваем точкам наш класс или оставляеа по умолчанию slick-dots
+    }
+  },
+  {
+    breakpoint: 576,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: true,
+     prevArrow: "<button type='button' class='arrow slick-prev'></button>",
+     nextArrow: "<button type='button' class='arrow slick-next'></button>",
+     dots: true, //убираем точки
+     dotsClass: 'slick-dots reviews-slider__dots', //присваиваем точкам наш класс или оставляеа по умолчанию slick-dots
+    }
+  },
+ ]
+});
